6fc73f86695533552eb8311d9d0d41814aa1f7da
Thomas Duval <thomas.duval@opmbx.org>
2016-02-19 15:43:09 +0100

    Switch to 'forEach' instead of 'for'

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,24 +1,22 @@
+/* global channels */
 /* global $ */
-
+channels = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "ogamingsc2", "comster404", "meteos"];
 
 $("#button").click(function () {
-    var channel = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "ogamingsc2", "comster404", "meteos"];
-    for (var j = 0; j < channel.length; j++) {
-        var url = 'https://api.twitch.tv/kraken/streams/' + channel[j];
-        $("#channel").append("<div class='jumbotron' id='" + channel[j] + "'>" + channel[j] + "</div>");
-        $('#' + channel[j]).append("<div>" + url + "</div>");
-        $.getJSON(url, function (data) {
-            //console.log(data.stream.game);
-            //$("#" + channel[j]).append("<div>" + data.stream.game + "</div>");
-            //$("#" + data.stream.channel.name).append("<div>" + data.stream.game + "</div>")
-            if (data.stream === null) {
-                $("#" + data.stream.channel.name).append("<div> null </div>");
-            } else {
-                $("#" + data.stream.channel.name).append("<div>" + data.stream.game + "</div>");
-                //$("#channel").append("<div class='jumbotron'>" + channel[j] + "<div>" + data.stream.game + "</div></div>"); 
-            
-            }
-        });
-
-    };
+    channels.forEach(twitchAPI)
 });
+
+function twitchAPI(element, index) {
+    var url = 'https://api.twitch.tv/kraken/streams/' + element;
+    $("#channels").append("<div class='jumbotron' id='" + element + "'><h4><a href='http://twitch.tv/" + element + "'>" + element + "</a></h4></div>");
+    $("#" + element).append("<div class='text-danger status'>Offline</div>");
+    $.getJSON(url, function (data) {
+        if (data.status == 422) {
+            console.log(data.status);
+            $("#" + element).children(".status").html("<div class='text-danger'>Account Closed</div>");
+        } else {
+            $("#" + data.stream.channel.name).children(".status").html("<div class='text-success'>" + data.stream.game + ": " + data.stream.channel.status + "</div>");
+        }
+    });
+}    
+
